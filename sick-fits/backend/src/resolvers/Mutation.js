7cf90bf0c0a08c9d50f126f50c05e779fd745b12
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2018-12-03 07:53:50 -0500

    Add code for lessons 12-14

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,25 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
 
-module.exports = mutations;
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    return item;
+  }
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   console.log(global.dogs);
+  //   return { name: args.name };
+  // }
+};
+
+module.exports = Mutations;
